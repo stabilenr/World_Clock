2aa32e469ae21fbf5444bcbe4f5555b8ab70f33d
stabilenr <stabilenr26@gmail.com>
2026-04-06 08:46:31 -0700

    Added the basic HTML

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,10 @@
+function getFlagEmoji(code) {
+  const codePoints = code
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
 
+let flagElement = document.querySelector("#flag");
+flagElement.innerHTML = getFlagEmoji("us");
