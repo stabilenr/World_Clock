b0104f3880df030256aa480e321939b839483a6d
stabilenr <stabilenr26@gmail.com>
2026-04-06 09:47:05 -0700

    Add CSS to style the project.

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,1 @@
-function getFlagEmoji(code) {
-  const codePoints = code
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
-}
 
-let flagElement = document.querySelector("#flag");
-flagElement.innerHTML = getFlagEmoji("us");
